aa55cfaafeaedab9895bb103852972403534808f
Antoine <antoine.carre@imt-atlantique.net>
2024-02-13 23:03:44 -0800

    Try code data

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+import * as d3 from 'd3';
+
+let Data = [];
+
+onMount(async () => {
+
+    const res = await fetch('static/Data/API_SP.DYN.LE00.IN_DS2_en_csv_v2_46.csv'); 
+
+    const csv = await res.text();
+
+    Data = d3.csvParse(csv, d3.autoType)
+
+    console.log(Data);
+
+});
+console.log(Data);
+
+</script>
+
+
+
+<h1>Welcome to the page</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
